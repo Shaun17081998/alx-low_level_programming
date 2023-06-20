896451b525eaa9520ff7c209816e4e21b7bb810c
Shaun <shaunmbatha17081998@gmail.com>
2023-06-20 09:08:14 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main is the entry point
+ * Description: prints the alphabet in lowercase, followed by a new line.
+ * Return: always zero
+ */
+int main(void)
+{
+	int lc = 97;
+
+	while (lc < 122)
+	{
+		if (lc == 101 || lc == 113)
+		{
+			lc++;
+			continue;
+		}
+		putchar(lc);
+		lc++;
+	}
+	putchar('\n');
+	return (0);
+}

6844a224f96f0d46f43ca9651e6039899ca648df
Shaun <shaunmbatha17081998@gmail.com>
2023-06-20 11:17:32 -0700

    9-print_com.c

---
+++ 0x01-variables_if_else_while/9-print_com.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main is the entry point
+ * prints all possible combinations of single-digit numbers
+ * Return: always zero
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar ('\n');
+		return (0);
+}

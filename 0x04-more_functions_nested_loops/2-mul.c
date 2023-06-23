15b3fa2f71ee18c3669f4cc439e15043541c1d08
Shaun <shaunmbatha17081998@gmail.com>
2023-06-23 05:02:51 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *-isdigit.c - Check if a character is a digit
+ *@x and y the two numbers to be multiplied
+ * Returns 1 if c is a digit
+ *Returns 0 otherwise
+*/
+
+int mul(int x, int y)
+
+{
+	return (x*y);
+}

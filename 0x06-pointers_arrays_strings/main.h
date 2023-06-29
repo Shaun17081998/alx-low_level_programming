20570aee0d176aab2e65333137614e754c55afbc
Shaun <shaunmbatha17081998@gmail.com>
2023-06-29 14:09:39 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -17,5 +17,7 @@ void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 char *_strncat(char *dest, char *src, int n);
+void print_numbers(void);
+int _putchar(char c);
 
 #endif
